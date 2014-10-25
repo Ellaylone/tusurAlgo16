86590c17ea04f32fbda4fcb5ef57e9c62aba4daa
Ellaylone <heamik91@yandex.ru>
2014-10-25 22:37:37 +0600

    работает для корня
    проблема либо с добавлением элементов к элементу не корню

--- tusurAlgo16.pas
+++ tusurAlgo16.pas
@@ -6,9 +6,8 @@ type
 		Left, Right: PTree;
 	end;
 var
-	Root: PTree;
-	TempName, TempDate: string;
-procedure insertInTree(var Root: PTree; InName: string; InDate: string);
+	Tree, Root: PTree;
+procedure insertInTree(var Tree: PTree; InName: string; InDate: string);
 
 	{ Дополнительная процедура, создающая и инициализирующая новый узел }
 	procedure CreateNode(var p: PTree; NodeName: string; NodeDate: string);
@@ -21,53 +20,63 @@ procedure insertInTree(var Root: PTree; InName: string; InDate: string);
 	end;
 	
 begin
-	if Root=NIL then CreateNode(Root, InName, InDate) { создаем новый узел дерева }
+	if Tree=NIL then CreateNode(Tree, InName, InDate) { создаем новый узел дерева }
 	else
-		if Root^.Left=NIL then begin
-			insertInTree(Root^.Left, InName, InDate);
-		end else if Root^.Right=NIL then begin 
-			insertInTree(Root^.Right, InName, InDate);
+		if Tree^.Left=NIL then begin
+			insertInTree(Tree^.Left, InName, InDate);
+		end else if Tree^.Right=NIL then begin 
+			insertInTree(Tree^.Right, InName, InDate);
 		end else writeln('This node already has 2 children');
 end;
-procedure walkTreeLR(p:PTree); 
+procedure walkTreeLR(var p: PTree); 
 Begin 
    if p<>nil then 
    begin 
-      walkTreeLR(p^.left); 
       writeln(p^.name, ' ', p^.birthDate); 
+      walkTreeLR(p^.left); 
       walkTreeLR(p^.right); 
    end; 
 end;
-procedure addChildrenByName(p: PTree; nodeName: string);
-var
-	inputName, inputDate: string;
+procedure addChildrenByName(var p: PTree; nodeName: string; TempName: string; TempDate: string);
 begin
+	writeln(p^.name);
 	if p<>nil then 
 	begin 
-		walkTreeLR(p^.left); 
 		if p^.name=nodeName then begin
-			writeln('Enter name');
-			readln(inputName);
-			writeln('Enter birth date');
-			readln(inputDate);
-			insertInTree(p, inputName, inputDate);
+			writeln(p^.name, ' ', p^.birthDate);
+			insertInTree(p, TempName, TempDate);
+			// if Tree^.name=nodeName then insertInTree(Root, TempName, TempDate);
 		end;
+		walkTreeLR(p^.left); 
 		walkTreeLR(p^.right); 
-	end; 
-	writeln('Not found');
+	end else writeln('Not found');
 end;
-procedure startTree(var Root: PTree);
+procedure makeTree(var Tree: PTree; var Root: PTree);
+var
+	TempName, TempDate, TargetName: string;
 begin
-	writeln('Enter name');
-	readln(TempName);
-	writeln('Enter birth date');
-	readln(TempDate);
-	insertInTree(Root, TempName, TempDate);
+	repeat
+		if Tree<>NIL then begin
+			writeln('Enter person whose parent we are adding(0 to stop)');
+			readln(TargetName);
+		end;
+		if TargetName='0' then break; {Если 0 - окончим ввод элементов}
+		writeln('Enter name');
+		readln(TempName);
+		writeln('Enter birth date');
+		readln(TempDate);
+		if Tree=NIL then begin
+			insertInTree(Tree, TempName, TempDate); 
+			Root := Tree;
+		end else begin
+			Tree := Root;
+			addChildrenByName(Tree, TargetName, TempName, TempDate);
+		end;
+	until false;
 end;
 begin
-	startTree(Root);
-	addChildrenByName(Root, 'name');
-	walkTreeLR(Root);
+	makeTree(Tree, Root);
+	walkTreeLR(Tree);
 	writeln('Done!');
 	readln();
 end.
